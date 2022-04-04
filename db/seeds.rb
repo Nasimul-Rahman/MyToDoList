c1011bd7453818e105f432ae9807e0f8bac38d00
Nasimul Rahman <nasimul@cynergon.net>
2022-04-04 13:33:09 +0600

    some seeds have been added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+p 'Destroying all seeds....'
+
+Task.destroy_all
+
 Task.create(title: 'Complete homework', completed: false, details: "do exercise 3 to 7 from the Trig chapter")
+Task.create(title: 'Go to the gym', completed: false, details: "Today is chest day")
+Task.create(title: 'Prep for meeting', completed: false, details: "Review data and logistics prior to meeting")
+Task.create(title: 'Go Surfing', completed: false, details: "You have surfing with Dave planned this afternoon")
+
+p "#{Task.count} tasks have been created"
