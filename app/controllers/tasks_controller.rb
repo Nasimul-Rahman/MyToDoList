c4dad2846305e3aca73c02604f5312b1bcb3f69b
Nasimul Rahman <nasimul@cynergon.net>
2022-04-05 23:40:28 +0600

    basic index page added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,23 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+    @new_task = Task.new(task_params)
+
+    if @task.save
+      redirect_to root, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
